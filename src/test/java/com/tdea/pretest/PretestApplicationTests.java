474458ec6760f7938f0b22b2935a8e4eb1526cb2
oquir <99086745+oquir@users.noreply.github.com>
2022-03-19 11:35:50 -0500

    Update PretestApplicationTests.java
    karen pineda acevedo

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -1,11 +1,18 @@
 package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+        String dbPassword = "pablo";
+        String password = "pablo";
+        UserUtils userUtils = new UserUtils();
+        assertTrue(userUtils.isPasswordEqual(dbPassword,password));
     }
 
 }
